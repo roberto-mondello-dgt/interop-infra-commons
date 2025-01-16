012a1bd299965c18515dac8136d717b5b7a6339d
mmorini-dxc <151859827+mmorini-dxc@users.noreply.github.com>
2025-01-16 11:49:54 +0100

    feat: sort env variables before kubectl diff (#15)

--- scripts/helm/diff.sh
+++ scripts/helm/diff.sh
@@ -9,6 +9,51 @@ else
   DIFF_ARGS+=("--color=auto")
 fi
 
+sort_env_section() {
+  target=$1
+  kind=""
+  fileName=""
+
+  if [[ -d $1 ]]; then
+
+    fileName=$(find $1 -name "*Deployment*")
+    if [[ -n $fileName ]]; then
+      fileName=$(basename $fileName)
+      kind="Deployment"
+
+    else
+      fileName=$(find $1 -name "*CronJob*")
+      if [[ -n $fileName ]]; then
+        fileName=$(basename $fileName)
+        kind="CronJob"
+
+      else
+        fileName=""
+        kind=""
+
+      fi
+    fi
+
+    target="$1/$fileName"
+  fi
+  
+  if [[ $kind == "Deployment" ]]; then
+    ORDERED_FLYWAY=$(KIND=$kind yq '(select(.kind == env(KIND)).spec.template.spec.initContainers[0].env | sort_by(.name))' "$target")
+    ORDERED=$(KIND=$kind yq '(select(.kind == env(KIND)).spec.template.spec.containers[0].env | sort_by(.name))' "$target")
+    
+    NEW_FLYWAY_ENVS=$ORDERED_FLYWAY yq -i '.spec.template.spec.initContainers[0].env = env(NEW_FLYWAY_ENVS)' $target
+    NEW_ENVS=$ORDERED yq -i '.spec.template.spec.containers[0].env = env(NEW_ENVS)' $target
+  
+  elif [[ $kind == "CronJob" ]]; then
+    ORDERED=$(KIND=$kind yq '(select(.kind == env(KIND)).spec.jobTemplate.spec.template.spec.containers[0].env | sort_by(.name))' "$target")
+    
+    NEW_ENVS=$ORDERED yq -i '.spec.jobTemplate.spec.template.spec.containers[0].env = env(NEW_ENVS)' $target
+  fi
+}
+
+sort_env_section $1
+sort_env_section $2
+
 SKIP_LINE=0
 diff "${DIFF_ARGS[@]}" "$@" | awk -v skip=$SKIP_LINE '
   BEGIN {
