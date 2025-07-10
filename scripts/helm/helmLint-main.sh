7b798e2760419c57b8878a3d2303f3ad3cae38f1
roberto-mondello-dgt <roberto.mondello@digitouch.it>
2025-07-10 14:45:15 +0200

    test

--- scripts/helm/helmLint-main.sh
+++ scripts/helm/helmLint-main.sh
@@ -103,17 +103,28 @@ done
 echo "Arguments: $@"
 
 # Normalize chart_path to Chart.yaml if it's a directory
+# if [[ -n "$chart_path" && -d "$chart_path" ]]; then
+#   if [[ -f "$chart_path/Chart.yaml" ]]; then
+#     chart_path="$chart_path/Chart.yaml"
+#   else
+#     echo "Error: Chart.yaml not found in directory '$chart_path'"
+#     exit 1
+#   fi
+# elif [[ ! -f "$chart_path" ]]; then
+#   echo "Error: Specified chart_path '$chart_path' does not exist"
+#   exit 1
+# fi
+
 if [[ -n "$chart_path" && -d "$chart_path" ]]; then
-  if [[ -f "$chart_path/Chart.yaml" ]]; then
-    chart_path="$chart_path/Chart.yaml"
-  else
+  if [[ ! -f "$chart_path/Chart.yaml" ]]; then
     echo "Error: Chart.yaml not found in directory '$chart_path'"
     exit 1
   fi
-elif [[ ! -f "$chart_path" ]]; then
-  echo "Error: Specified chart_path '$chart_path' does not exist as file"
+else
+  echo "Error: Specified chart_path '$chart_path' is not a directory"
   exit 1
 fi
+
 
 if [[ -z $environment || $environment == "" ]]; then
   echo "Environment cannot be null"
